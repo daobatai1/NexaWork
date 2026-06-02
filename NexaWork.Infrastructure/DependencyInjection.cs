ba088ddc39ae2fb4fd0a3587f6d921c8f550963c
daobatai1 <daobatai0@gmail.com>
2026-06-03 01:50:29 +0700

    feat(connection): implement connections between customers

--- NexaWork.Infrastructure/DependencyInjection.cs
+++ NexaWork.Infrastructure/DependencyInjection.cs
@@ -126,6 +126,7 @@ public static class DependencyInjection
         services.AddScoped<ICustomerSocialLinkRepository, CustomerSocialLinkRepository>();
         services.AddScoped<IReactionRepository, ReactionRepository>();
         services.AddScoped<IEducationRepository, EducationRepository>();
+        services.AddScoped<IConnectionRepository, ConnectionRepository>();
 
         // 4. Register your Services
         services.AddScoped<IFileStorageService, LocalFileStorageService>();
